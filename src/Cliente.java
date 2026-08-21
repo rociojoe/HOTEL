a30a4acea29a6f4337d1c7d25c54a4d07d704e09
joey <rhillm@miumg.edu.gt>
2026-08-21 17:09:25 -0600

    Clase cliente

---
+++ src/Cliente.java
@@ -0,0 +1,29 @@
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String telefono;
+
+    public Cliente(String nombre, String apellido, String cedula, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+}
